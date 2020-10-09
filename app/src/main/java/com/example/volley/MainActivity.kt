c33a3c38602fb2494dd8f31a7b514245915b6021
Kwounsu <sb00lgs@gmail.com>
2020-10-09 13:28:48 -0700

    Cancel a request

--- app/src/main/java/com/example/volley/MainActivity.kt
+++ app/src/main/java/com/example/volley/MainActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "TAG"
+    lateinit var stringRequest: StringRequest
+    lateinit var queue: RequestQueue
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,11 +22,11 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView)
 
         // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
+        queue = Volley.newRequestQueue(this)
         val url = "https://www.google.com"
 
         // Request a string response from the provided URL.
-        val stringRequest = StringRequest(
+        stringRequest = StringRequest(
             Request.Method.GET, url,
             { response ->
                 // Display the first 500 characters of the response string.
@@ -28,7 +34,16 @@ class MainActivity : AppCompatActivity() {
             },
             { textView.text = "That didn't work!" })
 
+        // Set the tag on the request.
+        stringRequest.tag = TAG
+
         // Add the request to the RequestQueue.
         queue.add(stringRequest)
+    }
+
+    override fun onStop() {
+        super.onStop()
+            super.onStop()
+            queue.cancelAll(TAG)
     }
 }
